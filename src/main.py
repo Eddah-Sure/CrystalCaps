f07a65233a8383c2bb181955301b43fc718f2ddd
Eddah <eddahsure@gmail.com>
2025-05-12 13:57:44 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,1 @@
-if __name__ == "__main__":
-    dataset_path = ""
 
-    model, results = run_spatial_gnn_capsnet(
-        dataset_path,
-        target_name="e_form",
-        epochs=epochs      
-    )
